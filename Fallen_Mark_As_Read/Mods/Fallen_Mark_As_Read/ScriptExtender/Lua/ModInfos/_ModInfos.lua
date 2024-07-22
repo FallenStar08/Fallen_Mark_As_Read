00d4240c0d2b6eab44768028ba90e63fc2338283
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-07-22 06:11:04 +0200

    Rainbow Message is back

--- Fallen_Mark_As_Read/Mods/Fallen_Mark_As_Read/ScriptExtender/Lua/ModInfos/_ModInfos.lua
+++ Fallen_Mark_As_Read/Mods/Fallen_Mark_As_Read/ScriptExtender/Lua/ModInfos/_ModInfos.lua
@@ -1,10 +1,5 @@
-local default_config={
-    LOG_ENABLED = 0,
-    MOD_ENABLED = 1,
-    DEBUG_MESSAGES = 3,
-    READ_BOOK_PREFIX = "[#",
-    READ_BOOK_SUFFIX = "#]",
-    UPDATE_RARITY = 1,
-}
+MOD_INFO = ModInfo:new("Fall_Mark_Books", "Fall_Mark_Books")
 
-MOD_INFO=ModInfo:new("Fall_Mark_Books","Fall_Mark_Books",true,default_config)
+BasicPrint(string.format("%s mod by FallenStar VERSION : %s starting up... ", "Mark Books As Read",
+        MOD_INFO.VERSION),
+    "INFO", nil, nil, true)
